992dbb523cfefb26fda311bd74b3b76ce7b34c15
zhang_yx <zhang_yx@gzhc365.com>
2020-11-25 20:48:39 +0800

    listdto支持构造模式

--- src/main/resources/template-pscp/center/param/dto/${className}ListDto.java
+++ src/main/resources/template-pscp/center/param/dto/${className}ListDto.java
@@ -19,6 +19,35 @@ public class ${className}ListDto extends BaseVo implements Serializable {
     <#list table.columns as column>
     @ApiModelProperty(value = "${column.remarks}")
     private ${column.simpleJavaType} ${column.columnNameLower};  
-    </#list>  
+    </#list>
+
+    public ${className}ListDto(){}
+
+    // 构造器
+    public ${className}ListDto(Builder b){
+        <#list table.columns as column>
+        ${column.columnNameLower} = b.${column.columnNameLower};
+        </#list>
+    }
+
+
+    public static class Builder {
+        <#list table.columns as column>
+        private ${column.simpleJavaType} ${column.columnNameLower};
+        </#list>
+
+     <#list table.columns as column>
+
+        public Builder set${column.columnName}(${column.simpleJavaType} ${column.columnNameLower}) {
+            this.${column.columnNameLower} = ${column.columnNameLower};
+            return this;
+        }
+
+    </#list>
+
+        public ${className}ListDto build(){
+            return new ${className}ListDto(this);
+        }
+    }
 
 }
